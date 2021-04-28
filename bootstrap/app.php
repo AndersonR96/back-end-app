7aad2dbb91c1982ad4c249ee63dbec8a4b207f52
AndersonR96 <anderrubiano@outlook.com>
2021-04-28 13:15:03 -0500

    update cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
+$app->configure('cors');
 /*
 |--------------------------------------------------------------------------
 | Return The Application
