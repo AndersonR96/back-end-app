48589706d8dec04cb3e7e1ca02ea0bfd73a865e6
AndersonR96 <anderrubiano@outlook.com>
2021-04-28 03:36:06 -0500

    update cors

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('users', 'UserController');
-Route::resource('departaments', 'DepartamentController');
+Route::group(['middleware' => ['cors']], function () {
+    Route::resource('users', 'UserController');
+    Route::resource('departaments', 'DepartamentController');
+});
