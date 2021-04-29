77a656b0fbdffc6673fb501708f013d21ae84e45
AndersonR96 <anderrubiano@outlook.com>
2021-04-28 22:07:26 -0500

    update cors

--- app/Departament.php
+++ app/Departament.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Departament extends Model
 {
 
-    public function users(){
-        return $this->hasMany('App\User')
-    }
     protected $fillable = [
         'departament',  
     ];
